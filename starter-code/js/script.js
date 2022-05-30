ce0ca023fff8e21f5b288ccf59da0d05e1a153f9
PO-KO <ayoub18daoui@gmail.com>
2022-05-30 17:49:26 +0100

    finish mobile version of destinition section

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -57,6 +57,34 @@ function switchDistance(activeDistance) {
       img.classList.add("active");
     }
   });
+
+  placeText.querySelectorAll("h1").forEach((name) => {
+    if (name.classList.contains("active")) {
+      name.classList.remove("active");
+      name.nextElementSibling.classList.remove("active");
+    }
+    if (name.dataset.place === activeDistance) {
+      name.classList.add("active");
+      name.nextElementSibling.classList.add("active");
+    }
+  });
+  placeDistance.querySelectorAll("span").forEach((distance) => {
+    if (distance.classList.contains("active")) {
+      distance.classList.remove("active");
+    }
+    if (distance.dataset.place === activeDistance) {
+      distance.classList.add("active");
+    }
+  });
+
+  placeTravelTime.querySelectorAll("span").forEach((travel) => {
+    if (travel.classList.contains("active")) {
+      travel.classList.remove("active");
+    }
+    if (travel.dataset.place === activeDistance) {
+      travel.classList.add("active");
+    }
+  });
 }
 
 function setAllDistances(data) {
@@ -64,11 +92,13 @@ function setAllDistances(data) {
     console.log(place.name.toLowerCase());
     let placeImg = document.createElement("div");
     placeImg.setAttribute("data-place", place.name.toLowerCase());
+
     if (placeImg.dataset.place === activeDistance) {
       placeImg.className = "place-img active";
     } else {
       placeImg.className = "place-img";
     }
+
     let img = document.createElement("img");
     img.src = place.images.png;
     img.alt = place.name;
@@ -77,18 +107,32 @@ function setAllDistances(data) {
     placeHeading.appendChild(placeImg);
 
     let name = document.createElement("h1");
+    name.setAttribute("data-place", place.name.toLowerCase());
     name.innerHTML = place.name;
     let description = document.createElement("p");
+    description.setAttribute("data-place", place.name.toLowerCase());
     description.innerHTML = place.description;
     placeText.appendChild(name);
     placeText.appendChild(description);
 
+    if (name.dataset.place === activeDistance) {
+      name.className = "active";
+      description.className = "active";
+    }
+
     let distance = document.createElement("span");
+    distance.setAttribute("data-place", place.name.toLowerCase());
     distance.innerHTML = place.distance;
     placeDistance.appendChild(distance);
 
-    let travel = document.createElement("h4");
+    let travel = document.createElement("span");
+    travel.setAttribute("data-place", place.name.toLowerCase());
     travel.innerHTML = place.travel;
     placeTravelTime.appendChild(travel);
+
+    if (name.dataset.place === activeDistance) {
+      distance.className = "active";
+      travel.className = "active";
+    }
   });
 }
