d78bf309c6de39fff452cfe7401d2400a0d4fb37
PO-KO <ayoub18daoui@gmail.com>
2022-05-30 14:54:24 +0100

    Finish destination mobile version without js

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -12,3 +12,40 @@ function showNav() {
 function closeNav() {
   nav.classList.remove("show");
 }
+
+// Get data from JSON file
+
+let request = new XMLHttpRequest();
+
+request.open("GET", "./data.json");
+request.send();
+
+request.onreadystatechange = () => {
+  if (request.status === 200 && request.readyState === 4) {
+    console.log(request.status);
+  }
+};
+
+// destination
+
+let placeSwitcher = document.querySelectorAll(".place-switcher l");
+let activeDistance = "moon";
+
+placeSwitcher.forEach((place) => {
+  place.addEventListener("click", setActiveDistance);
+});
+
+function setActiveDistance() {
+  console.log("hhhhh");
+  placeSwitcher.forEach((place) => {
+    place.classList.remove("active");
+  });
+  place.add("active");
+  activeDistance = place.dataset.place;
+
+  switchDistance(activeDistance);
+}
+
+function switchDistance(activeDistance) {}
+
+function setAllDistances() {}
